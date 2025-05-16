98939a32c31981ce05b12620e49239f2aae513d2
Ryette <ryette164@gmail.com>
2025-05-16 13:08:22 +0700

    push lan dau
    haskasd

---
+++ src/page/home.js
@@ -0,0 +1,13 @@
+// src/pages/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Hello World</h1>
+      <p>Chào mừng bạn đến với trang chủ!</p>
+    </div>
+  );
+};
+
+export default Home;
